e2aefefa8d7e6f0b8249f0725acff4234fbd6fff
Pratyush1705 <pratyushbalivada@gmail.com>
2023-01-19 10:36:08 +0530

    Sidebar commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import { Stack } from '@mui/material'
 import { categories } from '../utils/constants'
 
-const Sidebar=({selectCategory,setSelectCategory})=>{
+function Sidebar({selectCategory,setSelectCategory}) {
   
   return (
     <Stack direction="row" sx={{overflowY:"auto",height:{sx:'auto',md:'95%'},flexDirection:{md:'column'}}}>
         {categories.map((category)=>{
             return (<button className='category-btn' onClick={()=>setSelectCategory(category.name)} style={{color:'white',background:category.name===selectCategory&&'#FC1503'}} key={category.name}>
                 <span style={{color:category.name===selectCategory?'white':'red',marginRight:'15px'}}>{category.icon}</span>
-                <span style={{opacity:category.name===selectCategory?'1':'0.7'}}>{category.name}</span>
+                <span style={{opacity:category.name===selectCategory?'1':'0.7 '}}>{category.name}</span>
             </button>)
         })}
 
     </Stack>
   )
 }
+
 export default Sidebar
