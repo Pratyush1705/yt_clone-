9dc8951b06dce8167c3186d8519e8e6152c3b188
Pratyush1705 <pratyushbalivada@gmail.com>
2023-01-24 17:20:32 +0530

    Updated Feed and Channel Page

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import Videos from './Videos'
+import ChannelCard from './ChannelCard'
+import { fetchFrmApi } from '../utils/fetchFrmApi'
+//1:47:58
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  console.log(channelDetail, videos)
+  useEffect(() => {
+    fetchFrmApi(`channels?part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]));
+    fetchFrmApi(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items));
+  }, [id])
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{ background: 'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)', zIndex: 10, height: '300px' }} />
+        <ChannelCard channelDetail={channelDetail} marginTop='-110px' />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: '140px' } }} />
+        <Videos videos={videos} />
+      </Box>
+
+    </Box>
   )
 }
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Stack,Box } from '@mui/material'
-const Videos = ({videos}) => {
+import { Stack, Box } from '@mui/material'
+import VideoCard from './VideoCard'
+import ChannelCard from './ChannelCard'
+const Videos = ({ videos }) => {
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((item,idx)=>(
+      {videos.map((item, idx) => (
         <Box key={idx}>
-          
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
     </Stack>
